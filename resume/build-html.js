1bbacc37bb4f7c5168cf93c2d777453849d539ff
Steven Enten <steven@enten.fr>
2018-05-20 16:00:53 +0200

    build-html: add Open Graph metas

--- resume/build-html.js
+++ resume/build-html.js
@@ -20,22 +20,34 @@ function getResumeMarkup(ResumeComponent, jsonResume, cssFiles) {
   const css = getCssRaw(cssFiles);
   const resume = renderComponent(ResumeComponent, jsonResume);
 
+  const SITE_NAME = 'enten.fr';
   const CANNONICAL_URL = 'http://www.enten.fr/steven/';
+
+  const title = `${jsonResume.basics.name} | ${jsonResume.basics.label}`;
+  const metaDescription = `Resume of ${jsonResume.basics.name}, a ${jsonResume.basics.label.toLowerCase()} who works for ${jsonResume.work[0].company}`;
 
   return '<!doctype html>' + renderComponent(
     () => (
       <html lang="en">
         <head>
-          <title>{jsonResume.basics.name} | {jsonResume.basics.label}</title>
+          <title>{title}</title>
           <meta charSet="utf-8" />
-          <meta name="description" content={`Resume of ${jsonResume.basics.name}, a ${jsonResume.basics.label.toLowerCase()} who works for ${jsonResume.work[0].company}`} />
+          <meta name="description" content={metaDescription} />
+          <link rel="canonical" href={CANNONICAL_URL} />
+          <link rel="icon" href="favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="theme-color" content="#000000" />
-          <link rel="canonical" href={CANNONICAL_URL} />
-          <link rel="icon" href="favicon.ico" />
-          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+          <meta property="og:title" content={title} />
+          <meta property="og:site_name" content={SITE_NAME} />
+          <meta property="og:url" content={CANNONICAL_URL} />
+          <meta property="og:image" content={jsonResume.basics.picture} />
+          <meta property="og:locale" content="en-EN" />
+          <meta property="og:article:published_time" content="2005-01-01T00:00:00Z" />
+          <meta property="og:article:modified_time" content={(new Date()).toISOString()} />
+          <meta property="og:description" content={metaDescription} />
           <script type="application/ld+json" dangerouslySetInnerHTML={{__html: jsonLdResumeLdRaw}} />
+          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
           <style dangerouslySetInnerHTML={{__html: css}} />
         </head>
         <body dangerouslySetInnerHTML={{__html: resume}} />
